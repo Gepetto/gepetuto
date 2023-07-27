cf2399b39b35ad10e2673a56facb4952cbe03a05
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-07-27 15:51:09 +0200

    magic: make the linter happy

--- gepetuto/magic.py
+++ gepetuto/magic.py
@@ -1,39 +1,36 @@
-from IPython.core.magic import (
-    Magics,
-    line_magic,
-    magics_class,
-)
+"""Load / reload / hide code snippets from jupyter notebook."""
+from IPython.core.magic import Magics, line_magic, magics_class
 
 
 @magics_class
 class DoNotLoadMagics(Magics):
-    forceLoad = False
+    """Define a magic ipython command to help loading (or hiding) code snippets."""
+
+    force_load = False
 
     @line_magic
     def do_not_load(self, line):
-        if DoNotLoadMagics.forceLoad:
-            get_ipython().run_line_magic("load", line)
+        """Magic command to hide the snippet and let the student code by themself."""
+        if DoNotLoadMagics.force_load:
+            get_ipython().run_line_magic("load", line)  # noqa: F821
 
     @line_magic
     def force_load(self, line):
+        """Help the author to easily display even "do_not_load" snippets."""
         if line == "" or line == "on" or line == "True" or line == "1":
-            DoNotLoadMagics.forceLoad = True
+            DoNotLoadMagics.force_load = True
             print("Force load in ON")
         else:
-            DoNotLoadMagics.forceLoad = False
+            DoNotLoadMagics.force_load = False
             print("Force load is OFF")
 
 
-ip = get_ipython()
+ip = get_ipython()  # noqa: F821
 ip.register_magics(DoNotLoadMagics)
 
 
-def forceLoad(force=True):
-    DoNotLoadMagics.forceLoad = force
-
-
 print(
-    """NB: as for all the tutorials, a magic command %do_not_load is introduced """
-    """to hide the solutions to some questions. Change it for %load if you want to see """
-    """(and execute) the solution.""",
+    """NB: as for all the tutorials, a magic command %do_not_load is introduced to hide
+    the solutions to some questions. Change it for %load if you want to see (and
+    execute) the solution.""",
 )

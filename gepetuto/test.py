bbdd7cbc07a3a16ae1abc0b677a6c2e1007d0926
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-07-28 17:03:34 +0200

    improve pathlib use

--- gepetuto/test.py
+++ gepetuto/test.py
@@ -13,12 +13,10 @@ def test(**kwargs):
     LOG.info("testing tutorial sources.")
     for tp_number in range(100):
         LOG.debug(f"Looking for tp {tp_number}")
-        folder = Path() / f"tp{tp_number}"
-        if Path.exists(folder):
-            folder_files = os.listdir(folder)
-            folder_python_files = list(filter(lambda file: ".py" in file, folder_files))
-            for python_file in folder_python_files:
-                check_call(["python", folder / python_file])
+        folder = Path(f"tp{tp_number}")
+        if folder.exists():
+            for python_file in folder.glob("*.py"):
+                check_call([sys.executable, python_file])
         else:
             if tp_number == 0:
                 continue

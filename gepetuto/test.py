f32b85bde101ff01e726e35390c580249b3d879a
Théo Martinez <theo.martinez-fouche@laas.fr>
2023-08-01 17:06:01 +0200

    check ipynb files
    fix #8

--- gepetuto/test.py
+++ gepetuto/test.py
@@ -1,7 +1,6 @@
 """Add "test" action for the "gepetuto" program."""
 
 import logging
-import sys
 from pathlib import Path
 from subprocess import check_call
 from typing import List
@@ -17,5 +16,16 @@ def test(tp_id: List[int], **kwargs):
         folder = Path(f"tp{n}")
         for python_file in folder.glob("*.py"):
             LOG.debug(f"Checking {python_file}")
-            check_call([sys.executable, python_file])
+            check_call(["python", python_file])
+            check_ipynb(n)
     LOG.info("test passed.")
+
+
+def check_ipynb(tp_number):
+    """Check .ipynb files from given tp_number and move it in temporary folder."""
+    ipynb = next(Path().glob(f"{tp_number}_*.ipynb"))
+    check_call(["jupyter", "nbconvert", "--to", "script", f"{ipynb}"])
+    converted_ipynb = next(Path().glob(f"{tp_number}_*.py"))
+    LOG.debug(f"Checking temporary file {converted_ipynb}")
+    check_call(["python", converted_ipynb])
+    Path.unlink(converted_ipynb)

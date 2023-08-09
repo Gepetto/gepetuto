196ab0672931515f97e7c8c3bdf05d98d6a669cf
Théo Martinez <theo.martinez-fouche@laas.fr>
2023-08-09 10:39:53 +0200

    add new unit tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -11,7 +11,7 @@ class TestGepetutoArguments(unittest.TestCase):
     def test_no_arg(self):
         """Check files we work on when no arguments are given."""
         arguments = parse_args()
-        file_list = get_file_list(arguments.tp_id, arguments.file)
+        file_list = get_file_list(arguments)
         self.assertTrue(Path("tp1/cholesky.py") in file_list)
         self.assertTrue(Path("tp1/example_script.py") in file_list)
         self.assertTrue(Path("tp2/cholesky.py") in file_list)
@@ -20,7 +20,7 @@ class TestGepetutoArguments(unittest.TestCase):
     def test_tp_id_1(self):
         """Check files we work on when we specify tp_id = 1."""
         arguments = parse_args(["1"])
-        file_list = get_file_list(arguments.tp_id, arguments.file)
+        file_list = get_file_list(arguments)
         self.assertTrue(Path("tp1/cholesky.py") in file_list)
         self.assertTrue(Path("tp1/example_script.py") in file_list)
         self.assertTrue(Path("tp2/cholesky.py") not in file_list)
@@ -29,7 +29,7 @@ class TestGepetutoArguments(unittest.TestCase):
     def test_file_cholesky(self):
         """Check files we work on when we specify a file with --file."""
         arguments = parse_args(["--file", "tp1/cholesky.py"])
-        file_list = get_file_list(arguments.tp_id, arguments.file)
+        file_list = get_file_list(arguments)
         self.assertTrue(Path("tp1/cholesky.py") in file_list)
         self.assertTrue(Path("tp1/example_script.py") not in file_list)
         self.assertTrue(Path("tp2/cholesky.py") not in file_list)
@@ -38,7 +38,7 @@ class TestGepetutoArguments(unittest.TestCase):
     def test_tp_id_1_file_cholesky(self):
         """Check files we work on when we specify tp_id = 1 and a file in tp1 folder."""
         arguments = parse_args(["1", "--file", "tp1/cholesky.py"])
-        file_list = get_file_list(arguments.tp_id, arguments.file)
+        file_list = get_file_list(arguments)
         self.assertTrue(Path("tp1/cholesky.py") in file_list)
         self.assertTrue(Path("tp1/example_script.py") not in file_list)
         self.assertTrue(Path("tp2/cholesky.py") not in file_list)
@@ -47,7 +47,45 @@ class TestGepetutoArguments(unittest.TestCase):
     def test_no_file_matching(self):
         """Check files we work on when tp_id and --file has no files in common."""
         arguments = parse_args(["2", "--file", "tp1/cholesky.py"])
-        file_list = get_file_list(arguments.tp_id, arguments.file)
+        file_list = get_file_list(arguments)
+        self.assertTrue(Path("tp1/cholesky.py") not in file_list)
+        self.assertTrue(Path("tp1/example_script.py") not in file_list)
+        self.assertTrue(Path("tp2/cholesky.py") not in file_list)
+        self.assertTrue(Path("tp2/another_script.py") not in file_list)
+
+    def test_filter_cholesky(self):
+        """Check files we work on with --filter cholesky argument."""
+        arguments = parse_args(["--filter", "cholesky"])
+        file_list = get_file_list(arguments)
+        self.assertTrue(Path("tp1/cholesky.py") in file_list)
+        self.assertTrue(Path("tp1/example_script.py") not in file_list)
+        self.assertTrue(Path("tp2/cholesky.py") in file_list)
+        self.assertTrue(Path("tp2/another_script.py") not in file_list)
+
+    def test_tp_id_1_filter_cholesky(self):
+        """Check files we work on with --filter cholesky argument."""
+        arguments = parse_args(["1", "--filter", "cholesky"])
+        file_list = get_file_list(arguments)
+        self.assertTrue(Path("tp1/cholesky.py") in file_list)
+        self.assertTrue(Path("tp1/example_script.py") not in file_list)
+        self.assertTrue(Path("tp2/cholesky.py") not in file_list)
+        self.assertTrue(Path("tp2/another_script.py") not in file_list)
+
+    def test_file_cholesky_filter_cholesky(self):
+        """Check files we work on with --filter cholesky and --file on cholesky file."""
+        arguments = parse_args(["--file", "tp1/cholesky.py", "--filter", "cholesky"])
+        file_list = get_file_list(arguments)
+        self.assertTrue(Path("tp1/cholesky.py") in file_list)
+        self.assertTrue(Path("tp1/example_script.py") not in file_list)
+        self.assertTrue(Path("tp2/cholesky.py") not in file_list)
+        self.assertTrue(Path("tp2/another_script.py") not in file_list)
+
+    def test_no_file_matching_2(self):
+        """Check files we work on when --file and --filter has no files in common."""
+        arguments = parse_args(
+            ["--file", "tp1/example_script.py", "--filter", "cholesky"],
+        )
+        file_list = get_file_list(arguments)
         self.assertTrue(Path("tp1/cholesky.py") not in file_list)
         self.assertTrue(Path("tp1/example_script.py") not in file_list)
         self.assertTrue(Path("tp2/cholesky.py") not in file_list)

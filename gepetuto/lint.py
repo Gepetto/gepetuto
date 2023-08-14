769012a26ac07d5eaf0f8969d676bf4107672f11
Théo Martinez <theo.martinez-fouche@laas.fr>
2023-08-14 09:14:34 +0200

    add check argument
    fix #44

--- gepetuto/lint.py
+++ gepetuto/lint.py
@@ -11,13 +11,18 @@ def lint(files, **kwargs):
     LOG.info("linting tutorial sources.")
     for tp_files in files.values():
         for tp_file in tp_files:
-            lint_file(tp_file)
+            lint_file(tp_file, kwargs["check"])
     LOG.info("lint done.")
 
 
-def lint_file(file):
+def lint_file(file, check):
     """Lint python script."""
     LOG.debug(f"Checking {file}")
-    check_call(["isort", file])
-    check_call(["black", file])
-    check_call(["ruff", "--fix", file])
+    if check:
+        check_call(["isort", file, "--check"])
+        check_call(["black", file, "--check"])
+        check_call(["ruff", "--fix", file, "--exit-non-zero-on-fix"])
+    else:
+        check_call(["isort", file])
+        check_call(["black", file])
+        check_call(["ruff", "--fix", file])
